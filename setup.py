404120849d53d2716a22027ff53a2fe9b07733d2
Knox Hutchinson <knox@knoxsdata.com>
2020-11-11 02:40:54 +0000

    init commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(name='pypinger',
+      version='0.0.1',
+      description='Another way to ping in Python',
+      url='https://github.com/alessandromaggio/pythonping',
+      author='Knox Hutchinson',
+      author_email='me@alessandromaggio.com',
+      license='MIT',
+      packages=['pypinger'],
+      keywords=['ping', 'icmp', 'network'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: System Administrators',
+          'Natural Language :: English'
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      entry_points={  # Optional
+          'console_scripts': [
+              'pyping=pypinger:pyping',
+          ],
+      },
+      zip_safe=False)
